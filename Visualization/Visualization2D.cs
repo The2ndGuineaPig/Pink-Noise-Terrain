9ddcfa15990a650a7fb2c92cd54db075f082c2d8
Jumper-44 <89034685+Jumper-44@users.noreply.github.com>
2024-04-23 15:05:54 +0200

    Test

--- Visualization/Visualization2D.cs
+++ Visualization/Visualization2D.cs
@@ -3,9 +3,24 @@ using System;
 
 public partial class Visualization2D : TextureRect
 {
+	private Image image;
+	[Export] int imgWidth;
+	[Export] int imgHeight;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		GD.Print("Test");
+		image = Image.Create(imgWidth, imgHeight, false, Image.Format.Rgba8);
+
+		for (int i = 0; i < imgWidth; i++)
+		{
+			for (int j = 0; j < imgHeight; j++)
+			{
+				image.SetPixel(i, j, Colors.Black);
+			}	
+		}
+		
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
