a5b9b15af00c2b12ee40d16d802c1f33d18d2019
RedNickster <n.stenlokke@gmail.com>
2024-04-25 11:06:10 +0200

    SIMPLEX DUER!
    Simplex noise duer, man skal dog manuelt ind i koden for at angive x og y koordinat (Gør som i næste da kjærdsgaard skal burge det).

--- Simplex.cs
+++ Simplex.cs
@@ -5,12 +5,14 @@ using System.Collections.Generic;
 public partial class Simplex : Godot.Node
 {
     public override void _Ready()
-	{
-        GD.Print("Simplex noise test");
-        double x = 1.234;
-        double y = 5.678;
-        double noiseValue = Simplex.noise(x, y);
-        GD.Print($"Simplex noise at ({x}, {y}): {noiseValue}");
+	{   
+        
+        GD.Print("Redty kaldt");
+        InitializePermArray();
+        double x = 1;
+        double y = 5;
+        double noiseValue = noise(x, y);
+        GD.Print($"Simplex noise at: {noiseValue}");   
 	}
 
     private static int[][] grad3= new int[][]
@@ -21,14 +23,35 @@ public partial class Simplex : Godot.Node
         new int[] {0,-1,1}, new int[] {0,1,-1}, new int[] {0,-1,-1}
     };
 
+    private static int[] p = {151, 160, 137, 91, 90, 15,
+                          131, 13, 201, 95, 96, 53, 194, 233, 7, 225, 140, 36, 103, 30, 69, 142, 8, 99, 37, 240, 21, 10, 23,
+                          190, 6, 148, 247, 120, 234, 75, 0, 26, 197, 62, 94, 252, 219, 203, 117, 35, 11, 32, 57, 177, 33,
+                          88, 237, 149, 56, 87, 174, 20, 125, 136, 171, 168, 68, 175, 74, 165, 71, 134, 139, 48, 27, 166,
+                          77, 146, 158, 231, 83, 111, 229, 122, 60, 211, 133, 230, 220, 105, 92, 41, 55, 46, 245, 40, 244,
+                          102, 143, 54, 65, 25, 63, 161, 1, 216, 80, 73, 209, 76, 132, 187, 208, 89, 18, 169, 200, 196,
+                          135, 130, 116, 188, 159, 86, 164, 100, 109, 198, 173, 186, 3, 64, 52, 217, 226, 250, 124, 123,
+                          5, 202, 38, 147, 118, 126, 255, 82, 85, 212, 207, 206, 59, 227, 47, 16, 58, 17, 182, 189, 28, 42,
+                          223, 183, 170, 213, 119, 248, 152, 2, 44, 154, 163, 70, 221, 153, 101, 155, 167, 43, 172, 9,
+                          129, 22, 39, 253, 19, 98, 108, 110, 79, 113, 224, 232, 178, 185, 112, 104, 218, 246, 97, 228,
+                          251, 34, 242, 193, 238, 210, 144, 12, 191, 179, 162, 241, 81, 51, 145, 235, 249, 14, 239, 107,
+                          49, 192, 214, 31, 181, 199, 106, 157, 184, 84, 204, 176, 115, 121, 50, 45, 127, 4, 150, 254,
+                          138, 236, 205, 93, 222, 114, 67, 29, 24, 72, 243, 141, 128, 195, 78, 66, 215, 61, 156, 180};
+
 
     // To remove the need for index wrapping, double the permutation table length
     //private static int perm[] = new int[512];
     private static List<int> perm = new List<int>(512);
 
 
-    //static { for(int i=0; i<512; i++) perm[i]=p[i & 255]; }
-
+    // static { for(int i=0; i<512; i++) perm[i]=p[i & 255]; }
+    static void InitializePermArray()
+    {
+        for (int i = 0; i < 512; i++)
+        {
+            GD.Print(i);
+            perm.Add(p[i & 255]);
+        }
+    }
     private static int fastfloor(double x)
     {
         GD.Print("fastfloor nået");
@@ -98,8 +121,11 @@ public partial class Simplex : Godot.Node
         // Work out the hashed gradient indices of the three simplex corners
         int ii = i & 255;
         GD.Print("Hash gradient ii: " + ii);
+        GD.Print("Perm ii: " + perm[ii]);
         int jj = j & 255;
         GD.Print("Hash gradient jj: " + jj);
+        GD.Print("Perm ii: " + perm[jj]);
+        GD.Print(perm[ii + perm[jj]]);
         int gi0 = perm[ii + perm[jj]] % 12;
         GD.Print("Hash gradient gi0: " + gi0);
         int gi1 = perm[ii + i1 + perm[jj + j1]] % 12;
@@ -151,7 +177,7 @@ public partial class Simplex : Godot.Node
             n2 = t2 * t2 * dot(grad3[gi2], x2, y2);
             GD.Print("Linje 119, n2: " + n2);
         }
-
+        
         GD.Print("Linje 126, return: " + 70.0 * (n0 + n1 + n2));
         // Add contributions from each corner to get the final noise value.
         // The result is scaled to return values in the interval [-1,1].
