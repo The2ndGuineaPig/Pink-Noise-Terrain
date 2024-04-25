9c572aa3dd73d43a90b7d2e36b2635540654bb3e
Jumper-44 <89034685+Jumper-44@users.noreply.github.com>
2024-04-25 11:21:24 +0200

    Create and render img

--- Visualization/Visualization2D.cs
+++ Visualization/Visualization2D.cs
@@ -1,26 +1,30 @@
 using Godot;
 using System;
+using System.Runtime.CompilerServices;
 
-public partial class Visualization2D : TextureRect
+public partial class Visualization2D : Sprite2D
 {
 	private Image image;
-	[Export] int imgWidth;
-	[Export] int imgHeight;
+	[Export] private int imgWidth;
+	[Export] private int imgHeight;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		GD.Print("Test");
 		image = Image.Create(imgWidth, imgHeight, false, Image.Format.Rgba8);
 
 		for (int i = 0; i < imgWidth; i++)
 		{
 			for (int j = 0; j < imgHeight; j++)
 			{
-				image.SetPixel(i, j, Colors.Black);
-			}	
+				float R = 255.0f / (float)i;
+				float G = 255.0f / (float)i;
+				float B = 255.0f / (float)(i*j);
+				image.SetPixel(i, j, new Color(R, G, B));
+			}
 		}
-		
+
+		Texture = ImageTexture.CreateFromImage(image);
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
