b639109d851e2874c5b851f5e7907b74f29f824b
The2ndGuineaPig <61512604+The2ndGuineaPig@users.noreply.github.com>
2024-04-29 12:28:39 +0200

    Camera and img Rendering in progress

--- Interface/Camera2DModulate.cs
+++ Interface/Camera2DModulate.cs
@@ -9,70 +9,15 @@ public partial class Camera2DModulate : Control
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		// Called every time the node is added to the scene.
-		// Initialization here
-		// With anchor point in center set top left corner to 0,0
-		
-		
-		camera2D.AnchorMode = Camera2D.AnchorModeEnum.FixedTopLeft;
-		camera2D.Zoom = new Vector2(5, 5);
-		camera2D.LimitLeft = 0;
-		camera2D.LimitTop = 0;
+
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		// Called every frame
-		// Process here
-
-		// Move camera with arrow keys
-
-		if (Input.IsActionPressed("ui_right"))
-		{
-			camera2D.Position += new Vector2(1, 0);
-		}
-
-		if (Input.IsActionPressed("ui_left"))
-		{
-			camera2D.Position += new Vector2(-1, 0);
-		}
-
-		if (Input.IsActionPressed("ui_down"))
-		{
-			camera2D.Position += new Vector2(0, 1);
-		}
-
-		if (Input.IsActionPressed("ui_up"))
-		{
-			camera2D.Position += new Vector2(0, -1);
-		}
-
-		// Zoom in and out with scroll wheel
-
-
-		
-		
-
 
 	}
 
-	public override void _Input(InputEvent @event)
-	{
-		if (@event is InputEventMouseButton mouseButton)
-		{
-			if (mouseButton.ButtonIndex == MouseButton.WheelUp)
-			{
-				GD.Print(mouseButton);
-				camera2D.Zoom = camera2D.Zoom + new Vector2(0.1f, 0.1f);
-			}
-			else if (mouseButton.ButtonIndex == MouseButton.WheelDown)
-			{
-				GD.Print(mouseButton);
-				camera2D.Zoom = camera2D.Zoom - new Vector2(0.1f, 0.1f);
-			}
-		}
-	}
 }
 
 
